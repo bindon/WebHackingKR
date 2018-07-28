f4d0b14222b4a48b7cb38bcc216052f9be7efb31
bindon <bindon@Creator-PC>
2018-07-28 22:10:47 +0900

    Clear Challenge10

--- Challenge10.py
+++ Challenge10.py
@@ -4,10 +4,17 @@ import CookieManager
 
 challengeUrl = "http://webhacking.kr/challenge/codeing/code1.html?go=800"
 httpRequest = urllib2.Request(challengeUrl)
-CookieManager.addCookie("PHPSESSID=da0bd6cb852292c17cc2364c9dc6d334") # webhacking.kr에 로그인 하고 나온 cookie
+CookieManager.addCookie("PHPSESSID", "79d2e02ad592877ec33fb8651960469d")
 httpRequest.add_header("Cookie", CookieManager.getCookie())
 httpRequest.add_header("Referer", "http://webhacking.kr/challenge/codeing/code1.html")
-httpRequest.get_method = lambda: 'GET'
 
-httpResponse = urllib2.urlopen(httpRequest).read()
-print httpResponse
+httpConnection = None
+try:
+    httpConnection = urllib2.urlopen(httpRequest)
+    httpResponse = httpConnection.read()
+    print httpResponse
+except:
+    raise
+finally:
+    if httpConnection != None:
+        httpConnection.close()
