eaf793d259a14522f3bd0bbbd832fd9e042d4c2f
bindon <bindon@Creator-PC>
2018-07-29 01:05:44 +0900

    Clear Challenge20

---
+++ Challenge20.py
@@ -0,0 +1,9 @@
+#-*- coding: utf-8 -*-
+"""
+[*] 해당 문제는 자바스크립트를 입력하는 문제
+
+document.getElementsByName("id")[0].value="bindon";
+document.getElementsByName("cmt")[0].value="bindon's comment";
+document.getElementsByName("hack")[0].value=document.getElementsByName("attackme")[0].value;
+document.getElementsByName("lv5frm")[0].submit();
+"""
