34c737409a260fbec2d3f4affb7f2d7342c04a7e
bindon <bindon@Creator-PC>
2018-07-29 00:06:53 +0900

    Clear Challenge14

--- Challenge14.py
+++ Challenge14.py
@@ -7,7 +7,7 @@ challengeUrl = "http://webhacking.kr/index.php?mode=auth_go"
 parameter = urllib.urlencode({
     "answer": str(510*510)
 });
-httpRequest = urllib2.Request(challengeUrl)
+httpRequest = urllib2.Request(challengeUrl, data=parameter)
 CookieManager.addCookie("PHPSESSID", "5da1398a14fd19ed8ddcfb5ace4f7ac6")
 httpRequest.add_header("Cookie", CookieManager.getCookie())
 httpRequest.get_method = lambda: 'POST'
